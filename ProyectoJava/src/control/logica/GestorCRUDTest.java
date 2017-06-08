9d25dd4377a6aa1412273d0d2777a2dee82f6400
OscarPS11 <OscarPS11@DESKTOP-JGMG3VH>
2017-06-08 13:16:55 +0200

    gtg

---
+++ ProyectoJava/src/control/logica/GestorCRUDTest.java
@@ -0,0 +1,44 @@
+package control.logica;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GestorCRUDTest {
+
+	@Test
+	public void testGestorCRUD() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListaArticulo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListaPersona() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListaPedido() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDevuelveID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDameIDPedido() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEscribeLista() {
+		fail("Not yet implemented");
+	}
+
+}
